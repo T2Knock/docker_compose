25603ec8ce3694daa61d5ca6bbe953e9bdc7e5d6
knock-huynh <huynhtransongngoc@gmail.com>
2024-05-12 13:11:09 +0700

    refactor: remove version it's now obsolete

--- cubable/core/init/01.sql
+++
@@ -1,6 +0,0 @@
-create database if not exists `wgc_deploy`;
-create database if not exists `admin_panel`;
-create database if not exists `billing`;
-
-ALTER USER 'root'@'%' IDENTIFIED WITH mysql_native_password BY 'root';
-flush privileges;
