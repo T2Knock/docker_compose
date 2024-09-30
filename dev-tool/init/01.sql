7738af4cde9c9b3a3122b828f799fb6dd73582b9
ngochuynh <huynhtransongngoc@gmail.com>
2024-09-30 14:37:27 +0700

    feat(dev-tool): update docker script

--- dev-tool/init/01.sql
+++ dev-tool/init/01.sql
@@ -1,6 +1,5 @@
-create database if not exists `wgc_deploy`;
-create database if not exists `admin_panel`;
-create database if not exists `billing`;
+create database if not exists `co`;
+create database if not exists `dh`;
 
 ALTER USER 'root'@'%' IDENTIFIED WITH mysql_native_password BY 'root';
 flush privileges;
